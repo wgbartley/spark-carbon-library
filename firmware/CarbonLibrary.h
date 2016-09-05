f2cbd5332c984485ce39c372e67edcefe56b8a4a
Bert Cotton <bert.cotton@gmail.com>
2016-09-05 09:48:55 -0600

    Renamed cpp to conform with new name

--- firmware/CarbonLibrary.h
+++ firmware/CarbonLibrary.h
@@ -12,7 +12,7 @@ class SparkCarbon {
         bool begin(TCPClient * carbonClient, const char *carbonServer);
         bool sendData(String carbonMetric, String carbonValue, uint32_t carbonTimestamp);
         bool sendData(String carbonMetric, uint32_t carbonValue, uint32_t carbonTimestamp);
-    
+
     private:
         TCPClient * _carbonClient;
         IPAddress _carbonServer;
